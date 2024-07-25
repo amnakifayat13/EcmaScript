fd8a7de44895dda08c704681328b41cdf67c005f
amnakifayat13 <hayatullahamna@gmail.com>
2024-07-25 23:00:10 +0500

    Ecmascript

---
+++ first.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.a = void 0;
+var a = 10;
+exports.a = a;
---
+++ second.ts
@@ -0,0 +1,3 @@
+let b = 15;
+let c = 12;
+export {b ,c};
